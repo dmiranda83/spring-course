a666bd195e4d4023b59fbe71dcb8a7db26cf8651
dmiranda83 <diegoetata@gmail.com>
2020-03-23 19:19:27 -0300

    Initial commit

---
+++ src/main/java/com/springcourse/SpringCourseApplication.java
@@ -0,0 +1,12 @@
+package com.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseApplication.class, args);
+	}
+}
