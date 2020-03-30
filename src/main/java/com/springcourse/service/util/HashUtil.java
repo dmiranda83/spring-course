12dfa3b49ffbc9a60cb1451258de791437a402eb
dmiranda83 <diegoetata@gmail.com>
2020-03-30 18:48:45 -0300

    Create hash util

---
+++ src/main/java/com/springcourse/service/util/HashUtil.java
@@ -0,0 +1,10 @@
+package com.springcourse.service.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class HashUtil {
+
+	public static String getSecureHash(String text) {
+		return DigestUtils.sha256Hex(text);
+	}
+}
