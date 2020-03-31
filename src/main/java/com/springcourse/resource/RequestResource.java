11fc40cd2c6f7de89960ef4f9f4e34ef1476791e
dmiranda83 <diegoetata@gmail.com>
2020-03-31 19:16:11 -0300

    add listAllStagesById method

--- src/main/java/com/springcourse/resource/RequestResource.java
+++ src/main/java/com/springcourse/resource/RequestResource.java
@@ -14,34 +14,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
 import com.springcourse.service.RequestService;
+import com.springcourse.service.RequestStageService;
 
 @RestController
 @RequestMapping(value = "resquests")
 public class RequestResource {
 
 	@Autowired
-	private RequestService service;
+	private RequestService requestService;
+	@Autowired
+	private RequestStageService requestStageService;
 
 	@PostMapping
 	public ResponseEntity<Request> save(@RequestBody Request request) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(request));
+		return ResponseEntity.status(HttpStatus.CREATED).body(requestService.save(request));
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<Request> update(@PathVariable(name = "id") Long id, @RequestBody Request request) {
 		request.setId(id);
-		return ResponseEntity.ok(service.update(request));
+		return ResponseEntity.ok(requestService.update(request));
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Request> getById(@PathVariable(name = "id") Long id) {
-		return ResponseEntity.ok(service.getById(id));
+		return ResponseEntity.ok(requestService.getById(id));
+	}
+
+	@GetMapping("/{id}/request-stage")
+	public ResponseEntity<List<RequestStage>> listAllStagesById(@PathVariable(name = "id") Long id) {
+		return ResponseEntity.ok(requestStageService.listAllByRequestId(id));
 	}
 
 	@GetMapping
 	public ResponseEntity<List<Request>> getAll() {
-		return ResponseEntity.ok(service.listAll());
+		return ResponseEntity.ok(requestService.listAll());
 	}
 
 }
