dec5760812702d95d7d18b7f4d50b598f596b947
dmiranda83 <diegoetata@gmail.com>
2020-04-14 12:46:01 -0300

    Override argument not valid method

--- src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
@@ -1,20 +1,37 @@
 package com.springcourse.resource.exception;
 
 import java.util.Date;
+import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.springcourse.exception.NotFoundException;
 
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(NotFoundException.class)
 	public ResponseEntity<ApiError> handlerNotFoundException(NotFoundException ex) {
 		ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		BindingResult bindingResult = ex.getBindingResult();
+		List<ObjectError> allErrors = bindingResult.getAllErrors();
+		String deafultMessage = allErrors.get(0).getDefaultMessage();
+		ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), deafultMessage, new Date());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
